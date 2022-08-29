1cae8a09d1afca05d4f811035c694a790099ebb0
titof <hktitof@gmail.com>
2022-08-29 16:29:43 +0100

    set up .env

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,35 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    rotate: {
+      "-180": "-180deg",
+      "-90": "-90deg",
+      "-45": "-45deg",
+      0: "0",
+      45: "45deg",
+      90: "90deg",
+      135: "135deg",
+      180: "180deg",
+      270: "270deg",
+    },
+    extend: {
+      fontFamily: {
+        Header: ["Lato", "sans-serif"],
+        Text2: ["Lato", "sans-serif"],
+        About :["Playfair Display", "serif"]
+      },
+      colors: {
+        AAprimary: "#0b192f",
+        AAsecondary: "#64ffda",
+        AAtertiary: "#112340",
+        ResumeButtonHover: "#153040",
+        MobileNavBarColor: "#112340",
+        StartupBackground: "#020c1b",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [], 
+  variants: {
+    scrollbar: ["rounded"],
+  },
+};
